a40e344aa0b7e258bd13c35cff08246516344f7a
Pundreek Saurabh Srivastava <49810048+pundreek-saurabh@users.noreply.github.com>
2020-02-09 17:30:42 +0530

    Create script.py

---
+++ script.py
@@ -0,0 +1,18 @@
+from menu_item import MenuItem
+
+menu_item1 = MenuItem('Sandwich', 5)
+menu_item2 = MenuItem('Chocolate Cake', 4)
+menu_item3 = MenuItem('Coffee', 3)
+menu_item4 = MenuItem('Orange Juice', 2)
+
+menu_items = [menu_item1, menu_item2, menu_item3, menu_item4]
+
+
+index=0
+
+for menu_item in menu_items:
+    
+    print(str(index)+ '.' + menu_item.info())
+    
+    
+    index=index+1
