9655c5094ce2b0ad3f3e9504f47092fea256ef99
Pundreek Saurabh Srivastava <49810048+pundreek-saurabh@users.noreply.github.com>
2020-02-09 18:31:50 +0530

    Update script.py

--- script.py
+++ script.py
@@ -7,24 +7,23 @@ menu_item4 = MenuItem('Orange Juice', 2)
 
 menu_items = [menu_item1, menu_item2, menu_item3, menu_item4]
 
-
-index=0
+index = 0
 
 for menu_item in menu_items:
-    
-    print(str(index)+ '.' + menu_item.info())
-    
-    
-    index=index+1
-    
+    print(str(index) + '. ' + menu_item.info())
+    index += 1
+
 print('--------------------')
 
+order = int(input('Enter menu item number: '))
+selected_menu = menu_items[order]
+print('Selected item: ' + selected_menu.name)
 
-order= int(input('Enter menu item number: '))
+# Receive input from the console and set the count variable to it
+count= int (input ('Enter quantity (10% off for 3 or more): '))
 
+# Call the get_total_price method
+result =selected_menu.get_total_price(count4)
 
-selected_menu=menu_items[order]
-
-
-print('Selected item: '+ selected_menu.name)
-    
+# Output 'Your total is $____'
+print('Your total is $'+ str(result))
