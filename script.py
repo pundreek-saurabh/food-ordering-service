9b4c5d99af50921a2ebe54319b64093a4449ee51
Pundreek Saurabh Srivastava <49810048+pundreek-saurabh@users.noreply.github.com>
2020-02-09 19:51:40 +0530

    Update script.py

--- script.py
+++ script.py
@@ -1,29 +1,44 @@
-from menu_item import MenuItem
+from food import Food
+from drink import Drink
 
-menu_item1 = MenuItem('Sandwich', 5)
-menu_item2 = MenuItem('Chocolate Cake', 4)
-menu_item3 = MenuItem('Coffee', 3)
-menu_item4 = MenuItem('Orange Juice', 2)
+food1 = Food('Sandwich', 5, 330)
+food2 = Food('Chocolate Cake', 4, 450)
+food3 = Food('Cream Puff', 2, 180)
 
-menu_items = [menu_item1, menu_item2, menu_item3, menu_item4]
+foods = [food1, food2, food3]
 
+drink1 = Drink('Coffee', 3, 180)
+drink2 = Drink('Orange Juice', 2, 350)
+drink3 = Drink('Espresso', 3, 30)
+
+drinks = [drink1, drink2, drink3]
+
+print('Food')
 index = 0
+for food in foods:
+    print(str(index) + '. ' + food.info())
+    index += 1
 
-for menu_item in menu_items:
-    print(str(index) + '. ' + menu_item.info())
+print('Drinks')
+index = 0
+for drink in drinks:
+    print(str(index) + '. ' + drink.info())
     index += 1
 
 print('--------------------')
 
-order = int(input('Enter menu item number: '))
-selected_menu = menu_items[order]
-print('Selected item: ' + selected_menu.name)
+food_order = int(input('Enter food item number: '))
+selected_food = foods[food_order]
 
-# Receive input from the console and set the count variable to it
-count= int (input ('Enter quantity (10% off for 3 or more): '))
+drink_order = int(input('Enter drink item number: '))
+selected_drink = drinks[drink_order]
 
-# Call the get_total_price method
-result =selected_menu.get_total_price(count)
+# Take input from the console and assign it to the count variable
+count=int(input('How many meals would you like to purchase? (10% off for 3 or more):'))
 
-# Output 'Your total is $____'
-print('Your total is $'+ str(result))
+# Call the get_total_price method from selected_food and from selected_drink
+result=selected_food.get_total_price(count)+selected_drink.get_total_price(count)
+
+
+
+print('Your total is $' + str(result))
